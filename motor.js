4ef912a96a35c5be11db6c0f03be568974f9d8a9
Alex <meetalexjohnson@gmail.com>
2018-01-27 20:40:20 -0700

    Competition code

--- motor.js
+++ motor.js
@@ -1,59 +1,75 @@
 /**
  * This example is intended for dual H-bridge (quad half H-bridge) motor
  * driver ICs, like the Texas Instruments SN754410. It isn't intended for
- * motor driver breakout boards or more sophisticated drivers, which handle
+ * motor driver breakout boardAs or more sophisticated drivers, which handle
  * the PWM inversion for you.
  */
 
 var five = require("johnny-five"),
-  board = new five.Board();
+    boards = new five.Boards(["A", "B"]);
 
-board.on("ready", function() {
+  // boardA = new five.Board({port: "/dev/tty.usbserial-1410"});
+  // boardB = new five.Board({port: "/dev/tty.usbserial-1420"});
+
+var admin =  require("firebase-admin");
+
+var serviceAccount = require('./serviceaccount.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://iotdevfest2018.firebaseio.com/'
+});
+
+var db = admin.database();
+var motorsRef = db.ref("motor");
+
+//
+// Board B
+//
+boards.on("ready", function() {
+    console.log("ready board b");
+
   /**
    * Motor A: PWM 11, dir 12
    * Motor B: PWM 5, dir 4
    */
    var motors = new five.Motors([
-     { pins: { dir: 12, pwm: 11 }, invertPWM: true },
-     { pins: { dir: 4, pwm: 5}, invertPWM: true }
+     { pins: { dir: 12, pwm: 11, board: this.byId("A") }, invertPWM: true },
+     { pins: { dir: 4, pwm: 5, board: this.byId("A") }, invertPWM: true }
    ]);
 
-  board.repl.inject({
-    motors: motors
+motors.reverse(255);
+//
+//
+//
+  motorsRef.on("value", function(snapshot) {
+      let val = snapshot.val();
+
+      if(val === "left") {
+          console.log("inner val: ", val);
+          motors.forward(255);
+      } else if(val === "right") {
+          motors.reverse(255);
+          console.log("inner val: ", val);
+      } else {
+          motors.stop();
+      }
+  }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
   });
 
-  // Go forward at full speed for 5 seconds
-  console.log("Full speed ahead!");
-  motors.forward(255);
-  board.wait(5000, function () {
-    motors.stop();
-  });
 
-  // Go backwards at full speed for 5 seconds
-  console.log("Now backwards!");
-  motors.reverse(255);
-  board.wait(5000, function () {
-    motors.stop();
-  });
+    buttonA = new five.Button( {pin: 3, board: this.byId("B")});
+    buttonB = new five.Button( {pin: 6, board: this.byId("B")});
 
-  // Go left...
-  console.log("To the left!");
-  motors[0].reverse(200);
-  motors[1].forward(200);
-  board.wait(5000, function () {
-    motors.stop();
-  });
+    buttonA.on("down", function() {
+        console.log("A");
+        motorsRef.set("right");
+    });
 
-  // Go right...
-  console.log("To the right!");
-  motors[0].forward(200);
-  motors[1].reverse(200);
-  board.wait(5000, function () {
-    motors.stop();
-  });
-
-  // Use REPL if you want to go further
-  console.log("Done auto-driving! Use `motors` to control motors in REPL");
-
-});
-
+    // "down" the button is pressed
+    buttonB.on("down", function() {
+        console.log("B");
+        motorsRef.set("left");
+    });
+})
